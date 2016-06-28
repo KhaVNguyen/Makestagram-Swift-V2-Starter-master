c21d15587f8f3de3e4a161d75e67c4d7f8340b65
Kha <kha.vng@gmail.com>
2016-06-28 16:08:35 -0700

    Posts are sent to the timeline through queries based on either 1. The current user posted it or 2. The post is from a user that the current user is following

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -3,12 +3,34 @@ import Parse
 
 class TimelineViewController: UIViewController {
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     var photoTakingHelper: PhotoTakingHelper?
+    var posts: [Post] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tabBarController?.delegate = self
+        
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo: PFUser.currentUser()!)
+        
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        // combines the above queries, posts included if either requirements are met
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        query.includeKey("user")
+        query.orderByDescending("createdAt")
+        query.findObjectsInBackgroundWithBlock {(result: [PFObject]?, error: NSError?) -> Void in
+            self.posts = result as? [Post] ?? []
+            self.tableView.reloadData()
+        }
     }
 }
 
@@ -33,5 +55,17 @@ extension TimelineViewController: UITabBarControllerDelegate {
             post.image = image
             post.uploadPost()
         }
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource  {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell")!
+        cell.textLabel!.text = "Post"
+        return cell
     }
 }
