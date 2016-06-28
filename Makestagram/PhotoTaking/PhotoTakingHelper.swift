a1ab026c8bb134f38a06bc9a4937ce6391a1070f
Kha <kha.vng@gmail.com>
2016-06-28 10:44:56 -0700

    Added photo taking/selecting functionality
    - Callback to execute image posting code (to be implemented) after
    image is selected or taken
    - PhotoTakingHelper presents option to use the camera to take a picture
    or select an existing one from the user’s photo library

---
+++ Makestagram/PhotoTaking/PhotoTakingHelper.swift
@@ -0,0 +1,74 @@
+//
+//  PhotoTakingHelper.swift
+//  Makestagram
+//
+//  Created by Kha Nguyen on 6/27/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+typealias PhotoTakingHelperCallback = UIImage? -> Void
+
+class PhotoTakingHelper: NSObject {
+    // presents AlertViewController & UIImagePickerController
+    weak var viewController: UIViewController!
+    var callback: PhotoTakingHelperCallback
+    var imagePickerController: UIImagePickerController?
+    
+    init(viewController: UIViewController, callback: PhotoTakingHelperCallback) {
+        self.viewController = viewController
+        self.callback = callback
+        
+        super.init()
+        
+        showPhotoSourceSelection()
+    }
+    
+    func showPhotoSourceSelection() {
+        
+        // prompts user to choose between photo library or camera
+        let alertController = UIAlertController(title: nil, message: "Where do you want your picture from?", preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo from Library", style: .Default) { (action) in
+            self.showImagePickerController(.PhotoLibrary)
+        }
+            
+        alertController.addAction(photoLibraryAction)
+            
+        // only show camera option is rear camera is available
+        if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
+            let cameraAction = UIAlertAction(title: "Photo from Camera", style: .Default) { (action) in
+                self.showImagePickerController(.Camera)
+            }
+            alertController.addAction(cameraAction)
+        }
+        
+        viewController.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func showImagePickerController(sourceType: UIImagePickerControllerSourceType) {
+        imagePickerController = UIImagePickerController()
+        imagePickerController!.sourceType = sourceType
+        imagePickerController!.delegate = self
+        self.viewController.presentViewController(imagePickerController!, animated: true, completion: nil)
+    }
+}
+
+
+extension PhotoTakingHelper: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject: AnyObject]!) {
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+        
+        callback(image)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        viewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -1,35 +1,35 @@
-//
-//  TimelineViewController.swift
-//  Makestagram
-//
-//  Created by Kha Nguyen on 6/27/16.
-//  Copyright © 2016 Make School. All rights reserved.
-//
-
-import UIKit
+- import UIKit
 
 class TimelineViewController: UIViewController {
-
+    
+    var photoTakingHelper: PhotoTakingHelper?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tabBarController?.delegate = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
     }
-    */
 
+    func takePhoto() {
+        // instantiate PhotoTakingHelper class and provide callback when a photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            // nothing yet
+            print("received callback")
+        }
+    }
 }
